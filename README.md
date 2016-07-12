012065ad361174806511a0c60392d1ce6e996371
liam4 <towerofnix@gmail.com>
2016-07-12 16:02:08 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# indev.io
+
+..which we won't be hosting on indev.io!
---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict'
+
+console.log('Once upon a time there was a planet.')
+console.log('Then the planet exploded.')
+console.log('The end!')
